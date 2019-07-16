dd23ef5ca085a96325a855b344913158b5020cbe
maartencamps <52906716+maartencamps@users.noreply.github.com>
2019-07-16 08:04:03 +0200

    Bulk Create Teams

---
+++ Bulk Create Teams.ps1
@@ -0,0 +1,16 @@
+﻿# Bulk Create Teams
+# Maarten Camps
+
+[array]$teams = Import-Csv .\createteams.csv # Import CSV
+
+$prefix = "Team-" # Team Prefix
+
+foreach ($team in $teams) {
+
+    $t = $team.Name
+
+    $mailnickname = $prefix+$t #mailnickname is belangrijk om te vullen anders wordt deze random gegenereerd
+
+    New-Team -DisplayName $t -Description $t -MailNickName $mailnickname -AllowGuestCreateUpdateChannels $false -AllowGuestDeleteChannels $false -AllowDeleteChannels $false -AllowAddRemoveApps $false -AllowCreateUpdateRemoveTabs $false -AllowCreateUpdateChannels $false -AllowCreateUpdateRemoveConnectors $false -ShowInTeamsSearchAndSuggestions $true
+    #Set standard template attributes
+    }
